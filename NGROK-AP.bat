fbcdf1314c2a87248b7cd5d5a21eda3469f6dbb8
Thế Anh <haithuongpro2711@gmail.com>
2021-02-23 17:35:08 +0700

    Add files via upload

--- NGROK-AP.bat
+++ NGROK-AP.bat
@@ -1,3 +1,4 @@
+C:
 cd C:\Users\runneradmin\.ngrok2
 echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
 copy ngrok.yml C:\Windows\System32
