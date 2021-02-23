1942dc3ee2d326fe8852f042080899de1bb1415c
Thế Anh <haithuongpro2711@gmail.com>
2021-02-23 16:56:09 +0700

    Add files via upload

---
+++ NGROK-AP.bat
@@ -0,0 +1,1 @@
+echo authtoken: $Env:NGROK_AUTH_TOKEN > ngrok.yml & echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
