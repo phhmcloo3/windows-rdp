28d64ec8d4e7d3f9e6cd1b21c6cec53b0be98979
Thế Anh <haithuongpro2711@gmail.com>
2021-02-23 20:59:37 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,14 +1,13 @@
 @echo off
-net user administrator Thuonghai001 /add
-net localgroup administrators administrator /add
-net user administrator /active:yes
+sc start audiosrv >nul
+net user administrator Thuonghai001 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP.
 echo IP: https://dashboard.ngrok.com/status/tunnels
 echo User: Administrator
 echo Pass: Thuonghai001
-:check
-ping 127.0.0.1 > nul
-goto check
+
 
 
 
