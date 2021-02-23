560e8dc115a5dd93e9d93c59a262bb09881dec0b
Thế Anh <haithuongpro2711@gmail.com>
2021-02-23 21:20:13 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -4,7 +4,8 @@ net user administrator Thuonghai001 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP.
-echo IP: https://dashboard.ngrok.com/status/tunnels
+echo IP:
+curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 echo User: Administrator
 echo Pass: Thuonghai001
 
