fa00b5493c35c52d22581edca2b7c090a3eeb8c7
Thế Anh <haithuongpro2711@gmail.com>
2021-02-23 19:05:29 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -4,7 +4,7 @@ echo IP:
 echo curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
 echo Pass: Thuonghai001
-timeout /t 20 /NOBREAK > NUL
+pause
 goto check
 
 
