a700d798e618fa87e3db46967a30956df6b71efd
Thế Anh <haithuongpro2711@gmail.com>
2021-02-23 18:38:59 +0700

    Add files via upload

---
+++ NGROK-CHECK.bat
@@ -0,0 +1,13 @@
+:check
+echo All done! Connect your VM using RDP.
+echo IP:
+echo curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo User: Administrator
+echo Pass: Thuonghai001
+timeout 5
+goto check
+
+
+
+
+
